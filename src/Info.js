8100bb9c01e4abbbd7e29b74b8280ee901fec6ab
Solomon Yahwin <solomonyahwin@gmail.com>
2026-06-08 14:48:23 +0100

    chnaged the img, alighned button elements, fixed broken deployed img, etc

--- src/Info.js
+++ src/Info.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./style.css";
-import picture from "./images/download.jpg";
 
 export default function Info() {
   return (
     <div className="info">
-      <img src={picture} alt="my-name" />
+      <img src={`${process.env.PUBLIC_URL}/Solomon Yahwin1.jpeg`} alt="Solomon's Picture" />
       <h1>OKON, SOLOMON YAHWIN</h1>
       <h3>Front-end Developer</h3>
       <a href="https://www.some.com">Yahwin's Portfolio</a>
